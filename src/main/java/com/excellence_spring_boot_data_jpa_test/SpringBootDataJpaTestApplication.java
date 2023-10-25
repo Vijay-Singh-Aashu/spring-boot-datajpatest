fdcb28523b144be83f128db3928eed30c06ebb73
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-25 10:16:40 +0530

    base-code-created

---
+++ src/main/java/com/excellence_spring_boot_data_jpa_test/SpringBootDataJpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_data_jpa_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_data_jpa_test/SpringBootDataJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_data_jpa_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
